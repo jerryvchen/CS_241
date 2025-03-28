fde41317f74380d0fc953b478007d5effe26d957
Jerry Chen <jerryvchen1@gmail.com>
2025-03-28 10:35:58 -0700

    started wu2

---
+++ wu2/main.py
@@ -0,0 +1,13 @@
+##### Warm-Up 2 (wu2) Interpreter #####
+
+class WU2_Interpreter:
+    class WU2_Tokenizer:
+        pass
+    
+    def __init__(self):
+        # create identifier table
+
+        pass
+
+    def run(self):
+        input_str = input()
---
+++ wu2/test.py
@@ -0,0 +1,16 @@
+from main import WU2_Interpreter
+
+import unittest
+from unittest.mock import patch
+from io import StringIO
+
+class Test_W2_Interpreter(unittest.TestCase):
+    test1_str = 'computation var i <- 2 * 3; var abracadabra <- 7; (((abracadabra * i))); i - 5 - 1.'
+
+    @patch('builtins.input', return_value=test1_str)
+    def test_1(self, mocked_input):
+        wu2 = WU2_Interpreter()
+        wu2.run()
+
+if __name__ == '__main__':
+    unittest.main()

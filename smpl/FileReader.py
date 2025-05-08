387137b5ff15fe87a4138e127bc1adb4acdb866b
Jerry Chen <jerryvchen1@gmail.com>
2025-05-08 15:18:28 -0700

    begin main project

---
+++ smpl/FileReader.py
@@ -0,0 +1,16 @@
+"""
+FileReader.py
+
+
+"""
+
+class FileReader:
+    def __init__(self, fileName: str) -> None:
+        with open(fileName) as f:
+            pass
+    
+    def Error(errMsg: str) -> None:
+        pass
+
+    def GetNext() -> str:
+        pass

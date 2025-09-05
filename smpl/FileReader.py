b02995252c7499b1c95e9b8fa16af4bf7a1b6a01
Jerry Chen <69334968+jerryvchen@users.noreply.github.com>
2025-09-05 16:51:15 -0700

    code for FileReader

--- smpl/FileReader.py
+++ smpl/FileReader.py
@@ -5,6 +5,7 @@ Contains the implementation of the FileReader class.
 """
 
 from Exceptions import CompileError
+from typing import Generator
 
 class FileReader:
     """
@@ -13,6 +14,8 @@ class FileReader:
     Input is given as a input text file. Characters are parsed one-by-one 
     until an error or EOF is reached. 
     """
+    Error = 0   # enum type for error
+    EOF = 255   # enum type for end of file
 
     def __init__(self, file_name: str) -> None:
         """
@@ -22,6 +25,7 @@ class FileReader:
         :type file_name: str
         """
         self.file_name: str = file_name
+        self.is_error: bool = False
     
     def error(self, err_msg: str) -> None:
         """
@@ -30,19 +34,38 @@ class FileReader:
         :param err_msg: Error message to be shown.
         :type err_msg: str
         """
+        self.is_error = True
         raise CompileError(err_msg)
 
-    def get_next(self) -> str:
+    def get_next(self) -> str | int:
         """
-        Returns the next character in sequence from the file.
+        Returns the next character in sequence from the file. \n
 
         :return: Next character in the file.
         :rtype: str
         """
-        with open(self.file_name, 'r') as file:
-            ch: str | None = file.read(1)
-            while ch:
-                ch = file.read(1)
-                yield ch
+        if self.is_error: 
+            return FileReader.Error
+
+        ch: str = self.__get_next_util()
+        if ch == '':
+            return FileReader.EOF
+        
+        return ch
             
-    
+    def __get_next_util(self) -> Generator[str, None, None]:
+        """
+        Utility function/generator for the next character in
+        the file. Sends '' if end of file. 
+
+        :return: A generator for the next character in sequence
+        :rtype: Generator 
+        """
+        try:
+            with open(self.file_name, 'r') as file:
+                ch: str | None = file.read(1)
+                while ch:
+                    ch = file.read(1)
+                    yield ch
+        except Exception as e:
+            self.error(e)
